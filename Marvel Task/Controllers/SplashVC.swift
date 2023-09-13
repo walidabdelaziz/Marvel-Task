dc5e0a025253e6c2bc74ae3768f5aa874e20c69b
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2023-09-14 01:42:48 +0300

    add splash screen

---
+++ Marvel Task/Controllers/SplashVC.swift
@@ -0,0 +1,38 @@
+//
+//  SplashVC.swift
+//  Marvel Task
+//
+//  Created by Walid Ahmed on 14/09/2023.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    var count = 2
+    var timer: Timer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    func setupUI(){
+        self.timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(updateUI), userInfo: nil, repeats: true)
+    }
+    @objc func updateUI(){
+        if count > 0 {
+            count -= 1
+        }else{
+            showVC()
+            if timer != nil {
+                timer?.invalidate()
+                timer = nil
+            }
+        }
+    }
+    @objc func showVC(){
+
+    }
+}
