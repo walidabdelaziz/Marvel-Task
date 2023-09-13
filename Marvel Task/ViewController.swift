e5850da0559f9f6ab9d6f23e40388a6c01d3215f
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2023-09-14 01:02:48 +0300

    Initial Commit

---
+++ Marvel Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel Task
+//
+//  Created by Walid Ahmed on 14/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
